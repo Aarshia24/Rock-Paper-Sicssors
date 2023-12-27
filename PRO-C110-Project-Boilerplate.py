5fb90c3a55fbcbcc1637c4c0dc64aecfd7e54225
Aarshia24 <122778472+Aarshia24@users.noreply.github.com>
2023-12-27 02:13:28 +0100

    Add files via upload

---
+++ PRO-C110-Project-Boilerplate.py
@@ -0,0 +1,44 @@
+# To Capture Frame
+import cv2
+
+# To process image array
+import numpy as np
+
+# To Load the Pre-trained Model
+import tensorflow as tf
+
+camera = cv2.VideoCapture(0)
+
+# Infinite loop
+while True:
+ 
+	status , frame = camera.read()
+
+	if status:
+
+		frame = cv2.flip(frame , 1)
+
+		resized_frame = cv2.resize(frame , (224,224))
+
+		resized_frame = np.expand_dims(resized_frame , axis = 0)
+
+		resized_frame = resized_frame / 255
+
+		predictions = mymodel.predict(resized_frame)
+ 
+		rock = int(predictions[0][0]*100)
+		paper = int(predictions[0][1]*100)
+		scissor = int(predictions[0][2]*100)
+
+		print(f"Rock: {rock} %, Paper: {paper} %, Scissor: {scissor} %")
+
+		cv2.imshow('feed' , frame)
+
+		code = cv2.waitKey(1)
+		
+		if code == 32:
+			break
+
+camera.release()
+
+cv2.destroyAllWindows()
